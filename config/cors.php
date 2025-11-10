87992a01bfa0035b320e6ccb237258bc1480849e
KENT ADRIAN P. SABAYDAY <kentadrianpalaciosabayday@gmail.com>
2025-11-10 11:39:04 +0800

    Fix: Update CORS configuration to allow Vercel deployments

--- config/cors.php
+++ config/cors.php
@@ -5,18 +5,23 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
+    'allowed_origins' => array_filter([
         'http://localhost:3000',
         'http://127.0.0.1:3000',
-    ],
+        'https://localhost:3000',
+        env('FRONTEND_URL'),
+    ]),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '/^https:\/\/aura-learn-frontend-.*\.vercel\.app$/',
+        '/^https:\/\/.*-kenshis-projects-.*\.vercel\.app$/',
+    ],
 
     'allowed_headers' => ['*'],
 
     'exposed_headers' => ['Set-Cookie'],
 
-    'max_age' => 0,
+    'max_age' => 600,
 
     'supports_credentials' => true,
 ]; 
